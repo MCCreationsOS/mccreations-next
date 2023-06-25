2c9ee199a086ff625a261c47e38cbd2f642bdac3
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2023-06-25 10:50:29 -0700

    netlify test form w/ image

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,44 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="container">
+            <Head>
+                <title>Next.js Starter!</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-        <form netlify name="testForm" method="POST">
-          <input type="text" name="name"/>
-          <input type="file" name="logo"/>
-          <button>Submit</button>
-        </form>
-      </main>
+            <main>
+                <Header title="Welcome to my app!" />
+                <p className="description">
+                    Get started by editing <code>pages/index.js</code>
+                </p>
+                <form name="contact" method="POST" netlify>
+                    <p>
+                        <label>Your Name: <input type="text" name="name" /></label>
+                    </p>
+                    <p>
+                        <label>Your Email: <input type="email" name="email" /></label>
+                    </p>
+                    <p>
+                        <label>Your Role: <select name="role[]" multiple>
+                            <option value="leader">Leader</option>
+                            <option value="follower">Follower</option>
+                        </select></label>
+                    </p>
+                    <p>
+                        <label>Message: <textarea name="message"></textarea></label>
+                    </p>
+                    <p>
+                        <input type="image"></input>
+                    </p>
+                    <p>
+                        <button type="submit">Send</button>
+                    </p>
+                </form>
+            </main>
 
-      <Footer />
-    </div>
-  )
+            <Footer />
+        </div>
+    )
 }
