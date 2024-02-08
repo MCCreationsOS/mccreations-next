f32664c3e24759751b7a3558a6dc17c23378cbce
BenMeie <crazycowmm@gmail.com>
2024-02-07 17:30:09 -0800

    update creator card to link to profiles if linked to an account

--- components/cards/CreatorCard.tsx
+++ components/cards/CreatorCard.tsx
@@ -2,6 +2,7 @@ import defaultLogo from 'public/defaultLogo.png'
 import Image, { StaticImageData } from 'next/image';
 import { ICreator } from '@/app/types';
 import { getCreator } from '@/app/api/community';
+import Link from 'next/link';
 
 export default async function CreatorCard({creator}: {creator: ICreator}) {
     let image: string | StaticImageData = ""
@@ -14,12 +15,23 @@ export default async function CreatorCard({creator}: {creator: ICreator}) {
         image = "/defaultLogo.png"
     }
 
-    return (
-        <div className="creator_card">
-            <Image src={image} width={50} height={50} className='logo' alt={`${creator.username}'s logo`}></Image>
-            <div>
-                {creator.username}
+    if(creator.handle) {
+        return (
+            <Link className="creator_card" href={`/creator/${creator.handle}`}>
+                <Image src={image} width={50} height={50} className='logo' alt={`${creator.username}'s logo`}></Image>
+                <div>
+                    {creator.username}
+                </div>
+            </Link>
+        )
+    } else {
+        return (
+            <div className="creator_card">
+                <Image src={image} width={50} height={50} className='logo' alt={`${creator.username}'s logo`}></Image>
+                <div>
+                    {creator.username}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
