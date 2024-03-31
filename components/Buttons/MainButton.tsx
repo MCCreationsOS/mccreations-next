db940e608b031324752d0850ff5fc68c2bf31b8b
BenMeie <crazycowmm@gmail.com>
2024-03-31 11:11:21 -0700

    add support for additional css classes to MainButton

--- components/Buttons/MainButton.tsx
+++ components/Buttons/MainButton.tsx
@@ -1,7 +1,7 @@
 import styles from './buttons.module.css'
 
-export default function MainButton({onClick, children }: {onClick?: () => void, children: React.ReactNode}) {
+export default function MainButton({onClick, className, children }: {onClick?: () => void, className?: string, children: React.ReactNode}) {
     return (
-        <button type='button' onClick={onClick} className={styles.main_button}>{children}</button>
+        <button type='button' onClick={onClick} className={`${className} ${styles.main_button}`}>{children}</button>
     )
 }
