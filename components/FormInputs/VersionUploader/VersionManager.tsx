32dd4fe40ac73849fc25cd55eef7b9aa9dafa3e4
BenMeie <crazycowmm@gmail.com>
2024-04-28 12:35:50 -0700

    add delete option and fix copy down issue

--- components/FormInputs/VersionUploader/VersionManager.tsx
+++ components/FormInputs/VersionUploader/VersionManager.tsx
@@ -5,10 +5,11 @@ import FormComponent from "@/components/Form/Form"
 import { useCallback, useEffect, useState } from "react"
 import styles from './FileDropzone.module.css'
 import SecondaryButton from "@/components/Buttons/SecondaryButton"
-import { ChevronDown } from "react-feather"
+import { ChevronDown, Trash } from "react-feather"
 import Text from "../Text"
 import FileDropzone from "./FileUpload"
 import VersionUploader from "."
+import WarningButton from "@/components/Buttons/WarningButton"
 
 export default function VersionManager({onVersionsChanged, presetVersions}: {onVersionsChanged: (versions: string) => void, presetVersions?: string}) {
     const [versions, setVersions] = useState<IFile[]>([])
@@ -33,8 +34,8 @@ export default function VersionManager({onVersionsChanged, presetVersions}: {onV
             <div>
                 <div>
                     <SecondaryButton onClick={() => {
-                        setVersions([...versions, {type: 'map', contentVersion: "", minecraftVersion: "", worldUrl: "", resourceUrl: "", dataUrl: ""}])
-                        setCollapsed([...collapsed, false])
+                        setVersions([...versions, {type: 'map', contentVersion: "", minecraftVersion: "", worldUrl: "", resourceUrl: "", dataUrl: ""}, ])
+                        setCollapsed([...collapsed, false, ])
                     }}>Add Version</SecondaryButton>
                 </div>
                 {versions.map((version, idx) => {
@@ -42,13 +43,26 @@ export default function VersionManager({onVersionsChanged, presetVersions}: {onV
                         <div key={idx} className={`${styles.version} ${(collapsed[idx]) ? styles.collapsed : ""}`} >
                             <FormComponent id={`file${idx}`} onSave={(inputs) => {
                                 let v = [...versions];
+                                let worldUrl = inputs[2];
+                                let resourceUrl = inputs[4]
+                                let dataUrl = inputs[6]
+                                if(!worldUrl) {
+                                    worldUrl = inputs[3]
+                                }
+                                if(!resourceUrl) {
+                                    resourceUrl = inputs[5]
+                                }
+                                if(!dataUrl) {
+                                    dataUrl = inputs[7]
+                                }
+                                console.log(inputs)
                                 v[idx] = {
                                     type: v[idx].type,
                                     contentVersion: inputs[0] + "",
                                     minecraftVersion: inputs[1] + "",
-                                    worldUrl: inputs[2] + "",
-                                    resourceUrl: inputs[3],
-                                    dataUrl: inputs[4]
+                                    worldUrl: worldUrl + "",
+                                    resourceUrl: resourceUrl + "",
+                                    dataUrl: dataUrl + ""
                                 }
                                 setVersions(v)
                             }} > 
@@ -58,6 +72,7 @@ export default function VersionManager({onVersionsChanged, presetVersions}: {onV
                                 <VersionUploader name="Resource Pack" value={version.resourceUrl} />
                                 <VersionUploader name="Datapack" value={version.dataUrl} />
                             </FormComponent>
+                            <WarningButton onClick={() => {setVersions(versions.filter((v, i) => {return i !== idx}))}}>Delete</WarningButton>
                             <div className={`${styles.hide_version} ${(collapsed[idx]) ? styles.hide_reversed : ""}`} onClick={() => {let nc = [...collapsed]; nc[idx] = !nc[idx]; setCollapsed(nc)}}><ChevronDown /></div>
                         </div>
                     )
