7f35a8799bf2d824f907ac72734072c31b3c94e2
BenMeie <crazycowmm@gmail.com>
2024-06-22 11:41:13 -0700

    add icon button component

---
+++ components/Buttons/IconButton.tsx
@@ -0,0 +1,13 @@
+import styles from './buttons.module.css'
+
+/**
+ * A simple main button - Button Components are simple shortcuts to create buttons with predefined styles
+ * @param onClick The function to call when the button is clicked
+ * @param className Additional classes to apply to the button
+ * @param children The content of the button
+ */
+export default function IconButton({onClick, className, children }: {onClick?: () => void, className?: string, children: React.ReactNode}) {
+    return (
+        <button type='button' onClick={onClick} className={`${className} ${styles.icon_button}`}>{children}</button>
+    )
+}
