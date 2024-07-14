a3b3b0155272170241ff226a293419f4ff1b7617
BenMeie <crazycowmm@gmail.com>
2024-07-14 11:05:24 -0700

    fix error with transparent image in rich text editor

--- components/FormInputs/RichText/ImagePlugin.tsx
+++ components/FormInputs/RichText/ImagePlugin.tsx
@@ -192,12 +192,11 @@ export default function ImagesPlugin({
   return null;
 }
 
-const TRANSPARENT_IMAGE =
-  'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
-const img = document.createElement('img');
-img.src = TRANSPARENT_IMAGE;
-
 function $onDragStart(event: DragEvent): boolean {
+  const TRANSPARENT_IMAGE =
+  'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+  const img = document.createElement('img');
+  img.src = TRANSPARENT_IMAGE;
   const node = $getImageNodeInSelection();
   if (!node) {
     return false;
