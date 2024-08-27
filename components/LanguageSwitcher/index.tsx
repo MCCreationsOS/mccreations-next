8e2df33374f877b835a3282577cb0270c190c79b
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2024-08-26 19:45:24 -0700

    direct language switcher to new language submission

--- components/LanguageSwitcher/index.tsx
+++ components/LanguageSwitcher/index.tsx
@@ -2,32 +2,12 @@ import { useChangeLocale, useCurrentLocale } from "@/locales/client";
 import Link from "next/link";
 import { useState } from "react";
 import { Globe, Plus } from "react-feather";
-import { Popup } from "../Popup/Popup";
-import FormComponent from "../Form/Form";
-import Text from "../FormInputs/Text";
-import * as keys from '@/locales/langs/en_US'
 
 export default function LanguageSwitcher() {
     const changeLocale = useChangeLocale();
     const currentLocale = useCurrentLocale();
     const [open, setOpen] = useState(false);
-    const langKeys = Object.keys(keys.default)
 
-    const addLanguage = () => {
-        Popup.createPopup({
-            title: "Add A Language",
-            canClose: true,
-            useBackground: true,
-            content: <FormComponent id="add_language" onSave={saveNewLanguage} >
-                <Link href="">For help, click here</Link>
-                <br></br>
-                <br></br>
-                {langKeys.map((key) => {
-                    return <Text name={key} description={(key === 'language.code') ? <>An <Link href="https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes">ISO 639 language code</Link> follow by an <Link href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements">ISO 3166-1 Region Code</Link>. If you can't find your language, leave this blank.</> : undefined}/>
-                })}
-            </FormComponent>
-        })
-    }
 
     const saveNewLanguage = (inputs: string[]) => {}
 
@@ -42,7 +22,7 @@ export default function LanguageSwitcher() {
                     简体中文
                 </div>
                 <div className="option icon" style={{fontSize: "0.9rem"}}>
-                    <div onClick={addLanguage}><Plus /></div> <div onClick={addLanguage}> Add A Language</div>
+                    <Link href="/translate"><Plus /></Link> <Link href={"/translate"}> Add A Language</Link>
                 </div>
             </div>
         </div>
