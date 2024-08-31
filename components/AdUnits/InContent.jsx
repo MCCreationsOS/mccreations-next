33738c7afa01f33f659af8c6e7c7ef254a9beadf
BenMeie <crazycowmm@gmail.com>
2024-08-30 20:08:44 -0700

    refresh ads after 30 seconds

--- components/AdUnits/InContent.jsx
+++ components/AdUnits/InContent.jsx
@@ -8,11 +8,11 @@ import Script from "next/script"
 export default function InContentAdUnit() {
     const [isClient, setIsClient] = useState(false)
     const [adUnit, setAdUnit] = useState("")
- 
+
     useEffect(() => {
         let foundAdUnit = false
         adUnits.forEach(unit => {
-            if(!unit.inUse && adUnit.length === 0 && !foundAdUnit) {
+            if (!unit.inUse && adUnit.length === 0 && !foundAdUnit) {
                 console.log("Found ad unit not in use")
                 setAdUnit(unit.id)
                 unit.inUse = true
@@ -21,38 +21,53 @@ export default function InContentAdUnit() {
                 unit.inUse = false
                 setAdUnit("")
             }
-        
+
         })
         setIsClient(true)
     }, [])
     return (
         <div>
-        {isClient && adUnit && <>
-            <AdsenseComponent adSlot={adUnit} adFormat={"fluid"} adClient='ca-pub-5425604215170333' adLayout="-7p+f2-1p-4p+ez" width="100%" height="100%"/>
-        </>
-        }
-        <input type="hidden" value={adUnit + ""} />
+            {isClient && adUnit && <>
+                <AdsenseComponent adSlot={adUnit} adFormat={"fluid"} adClient='ca-pub-5425604215170333' adLayout="-7p+f2-1p-4p+ez" width="100%" height="100%" />
+            </>
+            }
+            <input type="hidden" value={adUnit + ""} />
         </div>
     )
 }
 
 
 export const AdsenseComponent = ({ adClient, adSlot, adFormat, adLayout, width, height }) => {
-  useEffect(() => {
-    try {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
+    const [render, setRender] = useState(true);
 
-  return (
-    <ins className="adsbygoogle"
-         style={{ display: 'block', width: width, height: height, margin: '0 auto' }}
-         data-ad-client={adClient}
-         data-ad-slot={adSlot}
-         data-ad-format={adFormat}
-         data-full-width-responsive="true"
-         data-ad-layout-key={adLayout}></ins>
-  );
+    useEffect(() => {
+        try {
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
+
+    useEffect(() => {
+        if(render) {
+            setTimeout(() => {
+                setRender(false)
+            }, 30000)
+        } else {
+            setTimeout(() => {
+                setRender(true)
+            }, 1)
+        }
+    }, [render])
+
+    if (!render) return null;
+    return (
+        <ins className="adsbygoogle"
+            style={{ display: 'block', width: width, height: height, margin: '0 auto' }}
+            data-ad-client={adClient}
+            data-ad-slot={adSlot}
+            data-ad-format={adFormat}
+            data-full-width-responsive="true"
+            data-ad-layout-key={adLayout}></ins>
+    );
 };
