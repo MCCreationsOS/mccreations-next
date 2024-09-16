cb76d790bbea18e16adf5b5583a016b61c882249
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2024-09-16 16:57:44 -0700

    opt file pages into static rendering

--- app/[locale]/datapacks/[slug]/files/page.tsx
+++ app/[locale]/datapacks/[slug]/files/page.tsx
@@ -13,8 +13,10 @@ import { useEffect, useState } from "react";
 import { Archive, Layers, Package, Server } from "react-feather";
 import styles from './table.module.css'
 import { useLocale, useTranslations } from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
 
 export default function Page({ params }: { params: Params }) {
+    unstable_setRequestLocale(params.locale)
     const [content, setContent] = useState<IContentDoc | null>(null)
     const t = useTranslations();
     const locale = useLocale()
