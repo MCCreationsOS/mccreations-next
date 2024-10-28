8748c0ac120bce400d64272612aa3c3284bbaf64
BenMeie <crazycowmm@gmail.com>
2024-10-28 14:45:09 -0700

    add install prompt

--- app/[locale]/settings/notifications/page.tsx
+++ app/[locale]/settings/notifications/page.tsx
@@ -13,6 +13,42 @@ import dynamic from "next/dynamic";
 
 const DropDown = dynamic(() => import("@/components/FormInputs/RichText/DropDown"), { ssr: false })
 
+function InstallPrompt() {
+    const [isIOS, setIsIOS] = useState(false)
+    const [isStandalone, setIsStandalone] = useState(false)
+   
+    useEffect(() => {
+      setIsIOS(
+        /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream
+      )
+   
+      setIsStandalone(window.matchMedia('(display-mode: standalone)').matches)
+    }, [])
+   
+    if (isStandalone) {
+      return null // Don't show install button if already installed
+    }
+   
+    return (
+      <div>
+        {isIOS && (
+          <p>
+            To use push notifications on iOS, tap the share button
+            <span role="img" aria-label="share icon">
+              {' '}
+              ⎋{' '}
+            </span>
+            and then "Add to Home Screen"
+            <span role="img" aria-label="plus icon">
+              {' '}
+              ➕{' '}
+            </span>.
+          </p>
+        )}
+      </div>
+    )
+  }
+
 
 export default function NotificationsPage() {
     const [isSupported, setIsSupported] = useState(false)
@@ -115,6 +151,7 @@ export default function NotificationsPage() {
                     </div>
                     <NotificationOptionDropdown type="translation" value={translation} />
                 </div>
+                <InstallPrompt />
             </div>
         </div>
     )
