4fd6cc644c650144668070387d4bfec7f6bd1101
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2024-10-28 16:03:09 -0700

    change subscribe to follow and add lang

--- components/Buttons/SubscribeButton.tsx
+++ components/Buttons/SubscribeButton.tsx
@@ -4,11 +4,13 @@ import { useCreator, useToken, useUser } from "@/app/api/hooks/users"
 import SecondaryButton from "./SecondaryButton"
 import { subscribe, unsubscribe } from "@/app/api/creators"
 import MainButton from "./MainButton"
+import { useTranslations } from "next-intl"
 
 export default function SubscribeButton({handle}: {handle: string}) {
     const { user } = useUser()
     const { creator } = useCreator(handle)
     const { token, setToken } = useToken()
+    const t = useTranslations()
 
     const handleSubscribe = () => {
         subscribe(token, handle)
@@ -19,6 +21,6 @@ export default function SubscribeButton({handle}: {handle: string}) {
     }
 
     if(user?.handle === handle) return null
-    if(user?.subscriptions?.includes(handle)) return <SecondaryButton onClick={handleUnsubscribe}>Unsubscribe</SecondaryButton>
-    return <MainButton onClick={handleSubscribe}>Subscribe</MainButton>
+    if(user?.subscriptions?.includes(handle)) return <SecondaryButton onClick={handleUnsubscribe}>{t('Profile.unfollow')}</SecondaryButton>
+    return <MainButton onClick={handleSubscribe}>{t('Profile.follow')}</MainButton>
 }
