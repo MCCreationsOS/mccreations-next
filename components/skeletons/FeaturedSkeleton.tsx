f213438b62ccffcbf6df3eef35c97a938b47e856
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2024-11-23 10:20:36 -0800

    fix featured skeleton to use new styles

--- components/skeletons/FeaturedSkeleton.tsx
+++ components/skeletons/FeaturedSkeleton.tsx
@@ -2,28 +2,29 @@ import { Link } from "@/app/api/navigation";
 import Image from "next/image";
 import { shimmer, toBase64 } from "./imageShimmer";
 import MainButton from '../Buttons/MainButton';
+import styles from '../FeaturedSlideshow/FeaturedSlideshow.module.css';
 
 export default function FeaturedSkeleton() {
     return (
-        <div className="big_slideshow featured">
-            <div className="slide active">
-                <Image className="image_background" src={`data:image/svg+xml;base64,${toBase64(shimmer(1920, 1080))}`} width={1920} height={1080} alt=""></Image>
-                <img className="nav_arrow left" src="/chev-left.svg"></img>
-                <img className="nav_arrow right" src="/chev-right.svg"></img>
-                <div className="information">
-                    <Image className="image" src={`data:image/svg+xml;base64,${toBase64(shimmer(1920, 1080))}`} width={1920} height={1080} alt={""}></Image>
-                    <h2 className="title"></h2>
-                    <p className="description"></p>
-                    <p className="author"></p>
-                    <div className='stats'>
-                        <div className="cardStat"><Image className="inTextIcon" src={"/download.svg"} alt="" width={16} height={16}></Image></div>
-                        <div className="cardStat"><Image className="inTextIcon" src={"/heart.svg"} alt="" width={16} height={16}></Image></div>
-                        <div className='cardStat'><Image className="inTextIcon" src={"/map.svg"} alt="" width={16} height={16}></Image></div>
+        <div className={styles.slideshow}>
+            <div className={`${styles.slide} ${styles.active}`}>
+                <Image className={styles.image_background} src={`data:image/svg+xml;base64,${toBase64(shimmer(1920, 1080))}`} width={1920} height={1080} alt=""></Image>
+                <img className={`${styles.nav_arrow} ${styles.left}`} src="/chev-left.svg"></img>
+                <img className={`${styles.nav_arrow} ${styles.right}`} src="/chev-right.svg"></img>
+                <div className={styles.information}>
+                    <Image className={styles.image} src={`data:image/svg+xml;base64,${toBase64(shimmer(1920, 1080))}`} width={1920} height={1080} alt={""}></Image>
+                    <h2 className={styles.title}></h2>
+                    <p className={styles.description}></p>
+                    <p className={styles.author}></p>
+                    <div className={styles.stats}>
+                        <div className={styles.cardStat}><Image className={styles.inTextIcon} src={"/download.svg"} alt="" width={16} height={16}></Image></div>
+                        <div className={styles.cardStat}><Image className={styles.inTextIcon} src={"/heart.svg"} alt="" width={16} height={16}></Image></div>
+                        <div className={styles.cardStat}><Image className={styles.inTextIcon} src={"/map.svg"} alt="" width={16} height={16}></Image></div>
                     </div>
                     <MainButton>See More!</MainButton>
                     <div>
-                        <div className="marker active">
-                            <span className="color" style={{margin: `0 -0%`}}></span>
+                        <div className={`${styles.marker} ${styles.active}`}>
+                            <span className={styles.color} style={{margin: `0 -0%`}}></span>
                         </div>
                     </div>
                 </div>
