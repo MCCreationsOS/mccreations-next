0681cf8f8c3b70036a733bb0c730b91ab559ee97
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2025-01-31 14:45:09 -0800

    convert dashboard to new api

--- components/Dashboard/Table.tsx
+++ components/Dashboard/Table.tsx
@@ -11,42 +11,18 @@ import { useRouter } from "next/navigation";
 import { getUser, useUserStore } from "@/app/api/auth";
 import { convertToType, deleteContent } from "@/app/api/content";
 import {useTranslations} from 'next-intl';
+import { useToken, useUserAlwaysSecure } from "@/app/api/hooks/users";
+import { useCreations } from "@/app/api/hooks/creations";
 
 export default function Table({collectionName}: {collectionName: CollectionNames}) {
-    const [maps, setMaps] = useState<IContentDoc[]>([])
-    const user = useUserStore() as IUser
-    const setUser = useUserStore((state) => state.setUser)
-    const router = useRouter();
+    const {user, isLoading} = useUserAlwaysSecure()
+    const {token} = useToken()
+    const {creations} = useCreations({contentType: collectionName, status: 0, limit: 20, page: 0, creators: [user?.handle ?? ""]})
     const contentType = convertToType(collectionName);
     const t = useTranslations()
 
-    useEffect(() => {
-        if(!user._id) {
-            getUser(localStorage?.getItem('jwt') + "").then((u) => {
-                if(u) {
-                    setUser(u)
-                }
-            })
-        }
-    }, [])
-
-    useEffect(() => {
-        if(user._id) {
-            getOwnedContent(localStorage?.getItem('jwt') + "")
-        }
-    }, [user])
-
-    const getOwnedContent = async (jwt: any) => {
-        fetch(`${process.env.DATA_URL}/${collectionName}-nosearch?status=0&limit=20&page=0&creator=${user?.handle}&sort=createdDate`, {
-            headers: {
-                authorization: jwt + ""
-            }
-        }).then((res) => {
-            res.json().then((data) => {
-                setMaps(data.documents)
-            })
-        })
-    }
+    if(isLoading) return <></>
+    console.log(user)
     
     return (
         <>
@@ -72,7 +48,7 @@ export default function Table({collectionName}: {collectionName: CollectionNames
                         <p>{t('Dashboard.rating')}</p>
                     </div>
                 </div>
-            {maps && maps.map((map: IContentDoc, idx) => (
+            {creations && creations.map((map: IContentDoc, idx) => (
                 <div className={styles.content_item} key={map.slug}>
                     <div className={styles.content_item_item}>
                         <Image className={styles.logo} src={map.images[0]} width={160} height={90} alt=""></Image>
@@ -83,17 +59,17 @@ export default function Table({collectionName}: {collectionName: CollectionNames
                         <div className={styles.info_buttons}>
                             <Link href={`/${contentType}s/${map.slug}`} title={t('Dashboard.view')}><ImageIcon /></Link>
                             <Link href={`/edit/${contentType}/${map.slug}`} title={t('Dashboard.edit')}><Edit /></Link>
-                            <span style={{color: 'red'}} onClick={() => {deleteContent(map._id, localStorage?.getItem('jwt'), collectionName); getOwnedContent(localStorage?.getItem('jwt'))}}><Trash /></span>
+                            <span style={{color: 'red'}} onClick={() => {deleteContent(map._id, localStorage?.getItem('jwt'), collectionName);}}><Trash /></span>
                         </div>
                     </div>
                     <div className={styles.content_item_item}>
                         <p>{(map?.status === 0) ? <span style={{color: "#c73030"}}>{t('Status.draft')}</span> : (map?.status === 1) ? <span style={{color: "#f0b432"}}>{t('Status.unapproved')}</span> : (map?.status === 2) ? <span style={{color: "#10b771"}}>{t('Status.approved')}</span>: <span style={{color:"#3154f4"}}>{t('Status.featured')}</span>}</p>
                     </div>
                     <div className={styles.content_item_item}>
-                        <p>{map.createdDate && new Date(map.createdDate).toLocaleDateString()}</p>
+                        <p>{map.createdDate && new Date(map.createdDate * 1000).toLocaleDateString()}</p>
                     </div>
                     <div className={styles.content_item_item}>
-                        <p>{map.updatedDate && new Date(map.updatedDate).toLocaleDateString()}</p>
+                        <p>{map.updatedDate && new Date(map.updatedDate * 1000).toLocaleDateString()}</p>
                     </div>
                     <div className={styles.content_item_item}>
                         <p>{map.downloads}</p>
