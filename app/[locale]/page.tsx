ef459a7140576dbb54cfd456018db5d20c2122c8
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2025-04-24 21:04:21 -0700

    small changes to the homepage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -21,13 +21,20 @@ export default async function Page({ params: { locale } }: { params: { locale: s
     const featured = (await searchContent({ contentType: "content", status: 3, limit: 5 }, false)).documents
     return (
         <>
+            <div className="bg-linear-to-b from-blue-500 to-blue-800 w-full mb-5">
+                <h1 className="text-white text-4xl font-brand text-center pt-7">MCCreations</h1>
+                <p className="text-white text-center text-xl pb-7">{t('Home.blurb')}</p>
+            </div>
             <FeaturedSlideshow creations={featured} />
             <div className="relative">
                 <AdsenseComponent adSlot={"3283646290"} adClient={"ca-pub-5425604215170333"} adFormat={"auto"} adLayout={undefined} width={"1000px"} height={"100px"}/>
             </div>
-            <UpdatedCreations />
-            <h2 className="view_all_header">{t('Home.ViewAll')}</h2>
-            <div className="view_all_buttons">
+            <div className="p-2">
+                <h2 className="text-2xl font-bold mt-5 mb-2">{t('Home.recently_updated')}</h2>
+                <UpdatedCreations />
+            </div>
+            <h2 className="w-full text-center text-2xl font-bold mt-5 mb-2">{t('Home.view_all')}</h2>
+            <div className="w-full flex justify-center gap-2 mb-10">
                 <Link href={`/maps`}><Button><Map />{t('map', { count: 2})}</Button></Link>
                 <Link href={`/datapacks`}><Button><Package />{t('datapack', { count: 2})}</Button></Link>
                 <Link href={`/resourcepacks`}><Button><Layers />{t('resourcepack', { count: 2})}</Button></Link>
