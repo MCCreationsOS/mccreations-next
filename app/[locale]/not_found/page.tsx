c42285cd1d0790b573d4ad77276b231dc0bd55c3
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2025-06-27 15:15:48 -0700

    update 404

--- app/[locale]/not_found/page.tsx
+++ app/[locale]/not_found/page.tsx
@@ -1,10 +1,13 @@
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import Link from "next/link";
 
 export default function NotFound({ params }: { params: Params}) {
     return (
         <>
-            <div className="centered-content">
-                <h1>Page not found</h1>
+            <div className="flex flex-col gap-2 items-center justify-center h-screen">
+                <h1 className="text-4xl font-bold">Page not found</h1>
+                <p className="text-lg">The page you are looking for does not exist.</p>
+                <Link href="/" className="text-blue-500">Go back to the home page</Link>
             </div>
         </>
     )
