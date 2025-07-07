23f82ee5e7fa0ffc6b697188ce1bea87fa0ebaef
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2025-07-07 14:31:20 -0700

    remove other languages for now

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,7 +2,7 @@ import {defineRouting} from 'next-intl/routing';
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en-US", 'zh-CN', 'ru-RU', 'fr-FR', 'de-DE', 'es-ES', 'ja-JP', 'ko-KR'],
+  locales: ["en-US"],
  localePrefix: 'always',
   // Used when no locale matches
   defaultLocale: 'en-US'
