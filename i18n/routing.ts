f7b6e49509b9e02c5cfcf8f8745e02a00be84b3a
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2025-07-14 08:06:39 -0700

    add languages

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,7 +2,7 @@ import {defineRouting} from 'next-intl/routing';
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en-US"],
+  locales: ["en-US", 'ja-JP', 'ko-KR', 'fr-FR', 'hi-IN', 'ru-RU'],
  localePrefix: 'always',
   // Used when no locale matches
   defaultLocale: 'en-US'
