b122a2eb75385a82b01ec6c8ec62e6b8d643e006
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2025-08-16 15:21:16 -0700

    change no-store in search request

--- app/api/content.ts
+++ app/api/content.ts
@@ -73,7 +73,7 @@ export async function searchContent(queryOptions: QueryOptions, count: boolean, 
     queryOptions = formatQueryOptions(queryOptions);
     try {
         let p1 = fetch(`${process.env.DATA_URL}/creations?contentType=${queryOptions.contentType}&status=${queryOptions.status}&limit=${queryOptions.limit}&page=${queryOptions.page}&sort=${queryOptions.sort}&search=${queryOptions.search}&sendCount=${count}&exclusiveStatus=${queryOptions.exclusiveStatus}&includeTags=${queryOptions.includeTags}&excludeTags=${queryOptions.excludeTags}&creators=${queryOptions.creators?.join(",")}`, {
-            cache: "no-store",
+            cache: "no-cache",
             method: 'GET',
             headers: {
                 authorization: token + ""
@@ -84,7 +84,7 @@ export async function searchContent(queryOptions: QueryOptions, count: boolean, 
             filterQuery = formatQueryOptions(filterQuery);
             // console.log(filterQuery)
             p2 = fetch(`${process.env.DATA_URL}/creations?contentType=${filterQuery.contentType}&status=${filterQuery.status}&limit=${filterQuery.limit}&page=${filterQuery.page}&sort=${filterQuery.sort}&search=${filterQuery.search}&sendCount=${count}&exclusiveStatus=${filterQuery.exclusiveStatus}&includeTags=${filterQuery.includeTags}&excludeTags=${filterQuery.excludeTags}&creators=${filterQuery.creators?.join(",")}`, {
-                cache: "no-store",
+                cache: "no-cache",
                 method: 'GET',
                 headers: {
                     authorization: token + ""
