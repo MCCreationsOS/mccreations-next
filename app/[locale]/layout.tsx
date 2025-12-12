317eb170714d90ae38a7f9031fb0c25bd99c231f
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2025-12-12 21:03:05 +0900

    completely remove g-ads connection

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,7 +4,7 @@ import Loading from './loading'
 import Footer from '@/components/Footer/Footer'
 import { Analytics } from "@vercel/analytics/react"
 import Script from 'next/script'
-import {NextIntlClientProvider} from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages, getTranslations, setRequestLocale } from 'next-intl/server'
 
 import '@/app/globals.css'
@@ -13,9 +13,9 @@ import ServiceWorkerManager from '@/components/ServiceWorkerManager'
 import { Toaster } from '@/components/ui/sonner'
 import { routing } from '@/i18n/routing'
 
-export async function generateMetadata({params}: {params: Promise<{locale: string}>}) {
-  const {locale} = await params;
-  const t = await getTranslations({locale: locale})
+export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale: locale })
   return {
     metadataBase: new URL('https://mccreations.net'),
     title: t('Pages.Home.Metadata.title'),
@@ -38,7 +38,7 @@ export async function generateMetadata({params}: {params: Promise<{locale: strin
       type: "website",
       url: "https://mccreations.net"
     },
-    keywords: [t('Pages.Home.Metadata.Keywords.minecraft'), t('Pages.Home.Metadata.Keywords.games'), t('Pages.Home.Metadata.Keywords.gaming'), t('Pages.Home.Metadata.Keywords.minecraft_map'), t('Pages.Home.Metadata.Keywords.minecraft_creations'), t('Pages.Home.Metadata.Keywords.minecraft_version', {minecraft_version: '1.21.8'}), t('Pages.Home.Metadata.Keywords.maps'), t('Pages.Home.Metadata.Keywords.minecraft_games'), t('Pages.Home.Metadata.Keywords.download')],
+    keywords: [t('Pages.Home.Metadata.Keywords.minecraft'), t('Pages.Home.Metadata.Keywords.games'), t('Pages.Home.Metadata.Keywords.gaming'), t('Pages.Home.Metadata.Keywords.minecraft_map'), t('Pages.Home.Metadata.Keywords.minecraft_creations'), t('Pages.Home.Metadata.Keywords.minecraft_version', { minecraft_version: '1.21.8' }), t('Pages.Home.Metadata.Keywords.maps'), t('Pages.Home.Metadata.Keywords.minecraft_games'), t('Pages.Home.Metadata.Keywords.download')],
     publisher: "MCCreations"
   }
 }
@@ -48,8 +48,8 @@ export function generateStaticParams() {
     locale: locale
   }))
 }
- 
-export default async function RootLayout(props: {params: Promise<{locale: string}>, children: React.ReactNode}) {
+
+export default async function RootLayout(props: { params: Promise<{ locale: string }>, children: React.ReactNode }) {
   const params = await props.params;
 
   const {
@@ -60,34 +60,33 @@ export default async function RootLayout(props: {params: Promise<{locale: string
     children
   } = props;
 
-  const messages = await getMessages({locale: locale})
+  const messages = await getMessages({ locale: locale })
 
   setRequestLocale(locale)
   console.log(locale)
   return (
 
-   <html lang={locale}>
-       <head>
-         <meta property="og:image:width" content="2500"></meta>
-         <meta property="og:image:height" content="1408"></meta>
-         <meta property="og:url" content="https://www.mccreations.net"></meta>
-         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-         <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Oxygen:wght@300;400;700&family=Paytone+One&display=swap" rel="stylesheet"></link>
-       </head>
-       <body id="view">
-         <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5425604215170333" crossOrigin="anonymous"></Script>
-         <NextIntlClientProvider locale={locale} messages={messages}>
-           <Menu />
-           <Suspense fallback={<Loading />}>
-               {children}
-           </Suspense>
-           <Footer params={props.params}></Footer>
-           <Toaster />
-           <Analytics />
-           <ServiceWorkerManager />
-         </NextIntlClientProvider>
-       </body>
-       </html>
-   )
+    <html lang={locale}>
+      <head>
+        <meta property="og:image:width" content="2500"></meta>
+        <meta property="og:image:height" content="1408"></meta>
+        <meta property="og:url" content="https://www.mccreations.net"></meta>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Oxygen:wght@300;400;700&family=Paytone+One&display=swap" rel="stylesheet"></link>
+      </head>
+      <body id="view">
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <Menu />
+          <Suspense fallback={<Loading />}>
+            {children}
+          </Suspense>
+          <Footer params={props.params}></Footer>
+          <Toaster />
+          <Analytics />
+          <ServiceWorkerManager />
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  )
 }
